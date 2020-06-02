2da5019c47f06d842a67b22e50ee737e7de7ba3a
titus buchanan <titusbuchananjr@gmail.com>
2020-06-02 17:48:22 -0400

    Added a post request that takes in data

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const { check,validationResult } = require('express-validator/check');
 const Profile = require('../../models/Profile');
 
 // @route       GET api/profile/me
@@ -16,10 +17,25 @@ router.get('/me', auth, async(req,res) => {
         }
         res.json(profile)
 
-    } catch(err){
+    } catch(err) {
         console.error(err.message);
         res.status(500).send('Server Error')
     }
 });
+
+// @route       POST api/profile
+// @description create or update user profile 
+// @acsess      Private
+
+router.post('/', [auth, [
+    check('status','Status is required').not().isEmpty(),
+    check('skills','Skills is required').not().isEmpty()
+]], async(req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors:errors.array() });
+    }
+
+})
 
 module.exports = router;
