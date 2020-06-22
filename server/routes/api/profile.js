f6df28cb44a20f3475718c8a1796aeecf62a1b72
titus buchanan <titusbuchananjr@gmail.com>
2020-06-22 00:31:58 -0400

    created route that grabs a users github profile and repos

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -326,8 +326,23 @@ router.delete('/education/:edu_id',auth, async (req,res) => {
 router.get('/github/:username', (req,res) => {
     try {
         const options = {
-            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get(githubClientID)}`
-        }
+          uri: `https://api.github.com/users/${
+            req.params.username
+          }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+            'githubClientID'
+          )}&client_secret=${config.get('gitHubSecret')}`,
+          method: 'GET',
+          headers: { 'user-agent': 'node.js' },
+        };
+        request(options, (error,response, body) => {
+            if(error) console.error(error);
+
+            if(response.statusCode !== 200) {
+                res.status(404).json({ msg:' No Github Profile Found'})
+            }
+
+            res.json(JSON.parse(body));
+        })
     } catch (error) {
         console.error(err.message);
         res.status(500).send('Server Error');
