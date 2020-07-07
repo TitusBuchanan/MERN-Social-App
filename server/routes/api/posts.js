e73ca2b7b71944763b42f10c94668e8dea6a9e2c
titus buchanan <titusbuchananjr@gmail.com>
2020-07-07 17:09:02 -0400

    added a route that deletes post by id

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -80,5 +80,33 @@ router.get('/:id', auth, async (req,res) => {
     }
 });
 
+// @route       DELETE api/posts /:id
+// @description Delete a post
+// @access      Private
+
+router.delete('/:id', auth, async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post){
+            return res.status(404).json({ msg:'Post Not FOund' });
+        }
+        
+        //Check on the user 
+        if(post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg:'User Not Authorized' });
+        }
+
+        await post.remove();
+
+        res.json({ msg:'Post removed' })
+    } catch (error) {
+        console.error(err.message);
+        if(err.kind === 'ObjectId'){
+            return res.status(404).json({ msg:'Post Not Found' })
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
 
 module.exports = router;
