6983baac3d7ad1849cf1699aeffa05014355759a
titus buchanan <titusbuchananjr@gmail.com>
2020-07-11 21:40:40 -0400

    unlike a post

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -120,9 +120,38 @@ router.put('/like/:id', auth, async(req,res)=> {
 
         //Check if post has already been liked
         if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
-            return res.json(400).json({ msg: 'Post Already Liked' })
+            return res.status(400).json({ msg: 'Post Already Liked' })
         }
         post.likes.unshift({ user: req.user.id })
+
+        await post.save();
+
+        res.json(post.likes);
+        
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+
+
+// @route       Put api/posts/unlike/:id
+// @description Unlike a Post
+// @access      Private
+
+router.put('/unlike/:id', auth, async(req,res)=> {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        //Check if post has already been liked
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({ msg: 'Post has not yet been liked' })
+        }
+        
+        //Get Removed Index
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+        post.likes.splice(removeIndex, 1);
 
         await post.save();
 
