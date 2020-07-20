a8f732c1e5737679575158f5046c192259be1ea8
titus buchanan <titusbuchananjr@gmail.com>
2020-07-20 14:54:15 -0400

    Added a reducer and action to trigger when logged out also created a edit profile form

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import React, { Fragment, useEffect }  from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
-import Landing from './components/layout/Landing'
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
-import Alert from './components/layout/Alert'
-//Redux
-import { Provider } from 'react-redux'
-import store from './store'
-import setAuthToken from './utils/setAuthToken'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 
-import { loadUser } from './actions/auth'
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import setAuthToken from './utils/setAuthToken';
+
+import { loadUser } from './actions/auth';
 
 if(localStorage.token) {
   setAuthToken(localStorage.token)
@@ -34,6 +39,9 @@ const  App = () => {
           <Switch>
             <Route exact path='/register' component = {Register} />
             <Route exact path='/login' component = {Login} />
+            <PrivateRoute exact path='/dashboard' component = {Dashboard} />
+            <PrivateRoute exact path='/create-profile' component = {CreateProfile} />
+            <PrivateRoute exact path='/edit-profile' component = {EditProfile} />
           </Switch>
         </section>
       </Fragment>
